0f6badde7aff8b468cb94f45f08d54391a8838cc
David Hasenjaeger <david.hasenjaeger@cayuse.com>
2019-01-03 16:53:05 -0800

    added better variable naming

---
+++ helpers.py
@@ -0,0 +1,32 @@
+def memoize(f):
+  cache = {}
+  def memoized_function(*args):
+    if hash(args) in cache:
+      return cache[hash(args)]
+    result = f(*args)
+    cache[hash(args)] = result
+    return result
+
+  return memoized_function
+
+def get_next_char(char):
+  if char is 'e':
+    return 'a'
+  
+  if char is 'd':
+    return 'f'
+  
+  if char is 'n':
+    return 'p'
+
+  if char is 'z':
+    return 'A'
+  
+  if char is 'Z':
+    return u'\u03b1'
+  
+  if isinstance(char, unicode):
+    print(char)
+    return unichr(ord(char) + 1)
+
+  return chr(ord(char) + 1)
---
+++ permutation_group.py
@@ -0,0 +1,58 @@
+import helpers
+
+@helpers.memoize
+def make_permutation_set(set_size):
+  if set_size <= 1:
+    return map(Permutation, [[0]])
+
+  if set_size == 2:
+    return map(Permutation, [[0, 1], [1, 0]])
+  
+  result = []
+  previous_permutation_set = make_permutation_set(set_size - 1)
+  first_element = 0
+  while first_element < set_size:
+    tail = []
+    while len(tail) < set_size - 1:
+      if len(tail) < first_element:
+        tail.append(len(tail))
+      else:
+        tail.append(len(tail) + 1)
+
+    for permutation in previous_permutation_set:
+      permuted_tail = permutation.apply(tail)
+      result.append(Permutation([first_element] + permuted_tail))
+    first_element += 1
+  return result
+
+class Permutation:
+  name = None
+
+  def __init__(self, perm):
+    self.perm = perm
+
+  def set_name(self, name):
+    self.name = name
+
+  def apply(self, permutation):
+    result = []
+    for value in self.perm:
+      result.append(permutation[self.perm[len(result)]])
+    return result
+
+class PermutationGroup:
+  def __init__(self, set_size):
+    self.elements = make_permutation_set(set_size)
+    self.set_names()
+
+  def set_names(self):
+    name = 'e'
+    for permutation in self.elements:
+      permutation.set_name(name)
+      name = helpers.get_next_char(name)
+
+  def get_elements_as_list(self):
+    return map(lambda el: el.perm, self.elements)
+
+  def get_element_names(self):
+    return map(lambda el: el.name, self.elements)
